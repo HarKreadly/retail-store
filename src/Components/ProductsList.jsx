6941427e3ffc483fcdd77c23ff5d04bb9e9aa4f1
HarKreadly <HarKreadly@users.noreply.github.com>
2024-11-30 16:08:12 +0100

    retail v0.8.2

--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -53,37 +53,39 @@ export default function ProductsList() {
     });
     } else {
       return(
-        <div className="grid bg-gray-800 border border-red-600 h-32 text-center items-center text-red-500 my-2 rounded">No Items</div>
+        <div className="grid bg-red-50 border border-red-200 h-32 text-center items-center text-red-500 my-2 rounded">No Items</div>
       )
     }
   };
 
   return (
     <div className="text-sm container mx-auto">
-        <div className="mx-4">
-        <h1>Searchy</h1>
-
-          <form onSubmit={(e)=>{e.preventDefault()}}>
-            <div className="relative">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-2.5 pointer-events-none ">
+      <div className="mx-4">
+        <p className="text-center text-5xl font-bold mb-6">Searchy</p>
+        <div className="flex justify-center">
+          <form onSubmit={(e) => e.preventDefault()} className="flex items-center gap-2">
+            <div className="relative flex">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-2.5 pointer-events-none">
                 <box-icon name="search" color="gray"></box-icon>
               </div>
-              <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-s-lg focus:ring-blue-500 focus:border-blue-500 w-1/3 ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleSearch} type="text" id="search" placeholder="Search" />
-              
-            <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-e-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleSearch} id="category">
-              <option value="">All</option>
-              {CategoryList.map((category)=> {
-                return <option value={category}>{category}</option>
-                })}
-            </select>
-            <input className="btn " onClick={()=> {setSearchValue(""); setSearchCatValue("")}} type="reset" />
+              <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-s-lg focus:ring-blue-500 focus:border-blue-500 w-80 ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleSearch} type="text" id="search" placeholder="Search"/>
+              <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-e-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleSearch} id="category">
+                <option value="">All</option>
+                {CategoryList.map((category) => (
+                  <option value={category} key={category}>
+                    {category.toUpperCase()}
+                  </option>
+                ))}
+              </select>
             </div>
+            <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 w-32 p-2.5 cursor-pointer dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onClick={() => { setSearchValue(""); setSearchCatValue(""); }} type="reset" value="Reset"/>
           </form>
         </div>
+      </div>
       
       <div className="mx-4">
       <h2>Products:</h2>
-        <div className="grid bg-slate-50 grid-cols-7 text-center rounded h-10 items-center">
+        <div className="grid bg-slate-50 grid-cols-7 text-center font-bold rounded h-10 items-center">
           <div>ID</div>
           <div>Image</div>
           <div>Title</div>
